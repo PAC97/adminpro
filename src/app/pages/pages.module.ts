3e04e814f0293452e540fe97848d78de8fb2c846
Pedro Cano <pedrinche27@gmail.com>
2019-08-27 22:19:59 -0600

    Barra de progreso funcional

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 
-
+import { FormsModule } from "@angular/forms";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
@@ -23,7 +23,8 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule
     ]
 })
 
