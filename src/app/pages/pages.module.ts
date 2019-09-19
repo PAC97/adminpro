abdb190ed027d284330f8fe54b8dbdba62577d9e
Emilia Robredo <emilia_robredo@hotmail.com>
2019-09-19 09:38:20 -0600

    Actualizacion

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,8 @@ import { SharedModule } from '../shared/shared.module';
 //servicios
 import { HttpClientModule } from "@angular/common/http";
 import {TipoUsuarioService} from './tipo-usuario/tipo-usuario.service';
+import { UsuarioService } from './usuario/usuario.service';
+import { TipoServicioService } from './tipo-servicio/tipo-servicio.service';
 //Paginacion
 import {NgxPaginationModule} from 'ngx-pagination';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
@@ -14,11 +16,18 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PAGES_ROUTES } from './pages.routes';
 import { UsuarioComponent } from './usuario/usuario.component';
-import {MenuComponent} from './menu/menu.component';
+import { MenuComponent} from './menu/menu.component';
 import { TipoUsuarioComponent } from './tipo-usuario/tipo-usuario.component';
 import { CommonModule } from '@angular/common';
 import { AddTipoUsuarioComponent } from './tipo-usuario/add-tipo-usuario/add-tipo-usuario.component';
 import { ModTipoUsuarioComponent } from './tipo-usuario/mod-tipo-usuario/mod-tipo-usuario.component';
+
+//
+import {ServicioComponent} from './tipo-servicio/tipo-servicio.component';
+import {AddTipoServicioComponent} from './tipo-servicio/add-servicio/add-tipo-servicio.component';
+import {ModTipoServicioComponent} from './tipo-servicio/mod-servicio/mod-tipo-servicio.component'
+import { from } from 'rxjs';
+import { ModUsuarioComponent } from './usuario/mod-usuario/mod-usuario.component';
 
 @NgModule({
     declarations: [
@@ -30,7 +39,11 @@ import { ModTipoUsuarioComponent } from './tipo-usuario/mod-tipo-usuario/mod-tip
         MenuComponent,
         TipoUsuarioComponent,
         AddTipoUsuarioComponent,
-        ModTipoUsuarioComponent
+        ModTipoUsuarioComponent,
+        ModUsuarioComponent,
+        ServicioComponent,
+        AddTipoServicioComponent,
+        ModTipoServicioComponent
     ], exports: [
         PagesComponent,
         DashboardComponent,
@@ -47,7 +60,9 @@ import { ModTipoUsuarioComponent } from './tipo-usuario/mod-tipo-usuario/mod-tip
         AngularFontAwesomeModule
    ],
    providers: [
-    TipoUsuarioService
+    TipoUsuarioService,
+    UsuarioService,
+    TipoServicioService
    ]
 })
 
